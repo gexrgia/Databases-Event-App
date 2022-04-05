404ffa6733517359f71f46d072157d9c2683228d
Jess <70274122+J-Compsci@users.noreply.github.com>
2022-04-05 15:11:44 -0400

    Swap html tables to boostrap div grid
    Some basic JS functionality

--- js/eventCode.js
+++ js/eventCode.js
@@ -16,7 +16,7 @@ function loginUser() {
         email:email, password:password
     });
 
-   // var url; /*[link to API --- Depends on API]*/
+  // var url; /*[link to API --- Depends on API]*/
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -44,4 +44,43 @@ function loginUser() {
     } catch(err) {
         displayError(err.message);
     }
-  }
+}
+
+function newComment(){
+  var showComment = document.getElementById("newCommentSection");
+  showComment.style.display == "none" ? showComment.style.display = "block" : showComment.style.display = "none";
+}
+function postComment(){}
+function searchEvent(){}
+function addEvent(){}
+function viewEvent(){}
+
+function openEdit(button, cid) {
+  curRow = button.parentNode.parentNode;
+  curEvent = cid;
+
+  var cols = curRow.getElementsByTagName("div");
+  var group = cols[0].getElementsByTagName("div");
+
+  document.getElementById("eName").value = group[0].innerHTML;
+  document.getElementById("eDate").value = group[1].innerHTML;
+  document.getElementById("eLocation").value = group[2].innerHTML;
+  document.getElementById("eDescription").value = group[3].innerHTML;
+
+
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("editForm").style.display = "block";
+}
+
+function closeEdit() {
+  curRow = null;
+  curEvent = 0;
+
+  document.getElementById("eName").value = "";
+  document.getElementById("eDate").value = "";
+  document.getElementById("eLocation").value = "";
+  document.getElementById("eDescription").value = "";
+
+  document.getElementById("editForm").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
