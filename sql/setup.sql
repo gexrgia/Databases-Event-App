06932f9364df186d89d291ca4239a51224f9a0f7
tristanjp12 <tristanjaramillopritchard@gmail.com>
2022-04-06 18:08:58 -0400

    changed category to comment

--- sql/setup.sql
+++ sql/setup.sql
@@ -18,7 +18,7 @@ CREATE TABLE `events` (
   `name` varchar(100) NOT NULL,
   `time` datetime NOT NULL,
   `location` varchar(190) NOT NULL,
-  `category` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
   `description` text NOT NULL)
  ENGINE=InnoDB;
 
